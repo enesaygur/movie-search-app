ed050071255eb43ed7be009fc568d738470774a0
Enes <enesaygur387@gmail.com>
2026-06-19 15:27:08 +0300

    feat: add movie grid layout

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,14 +1,20 @@
 import type { Movie } from "../types/movie";
+import styles from "./MovieCard.module.css";
 
 type MovieCardProps = {
   movie: Movie;
 };
 function MovieCard({ movie }: MovieCardProps) {
   return (
-    <div>
-      <img src={movie["#IMG_POSTER"]} alt={movie["#TITLE"]} width={150} />
-      <h3>{movie["#TITLE"]}</h3>
-      <p>{movie["#YEAR"]}</p>
+    <div className={styles.card}>
+      <img
+        src={movie["#IMG_POSTER"]}
+        alt={movie["#TITLE"]}
+        width={150}
+        className={styles.image}
+      />
+      <h3 className={styles.title}>{movie["#TITLE"]}</h3>
+      <p className={styles.year}>{movie["#YEAR"]}</p>
     </div>
   );
 }
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,15 +1,16 @@
 import type { Movie } from "../types/movie";
 import MovieCard from "./MovieCard";
+import styles  from "./MovieList.module.css";
 type MovieListProps = {
   movies: Movie[];
 };
 function MovieList({ movies }: MovieListProps) {
   return (
-    <ul>
+    <div className={styles.container}>
       {movies.map((movie) => (
         <MovieCard key={movie["#IMDB_ID"]} movie={movie} />
       ))}
-    </ul>
+    </div>
   );
 }
 
