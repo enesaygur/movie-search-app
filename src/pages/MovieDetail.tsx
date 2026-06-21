a6cbf44a22180bc1b7ea1750f8bc88f8b64a157a
Enes <enesaygur387@gmail.com>
2026-06-21 14:51:13 +0300

    feat: add back navigation to movie detail page

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import type { Movie } from "../types/movie";
 import { getMovieById } from "../services/movieService";
 
@@ -8,6 +8,7 @@ function MovieDetail() {
   const [movie, setMovie] = useState<Movie | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const navigate = useNavigate();
   useEffect(() => {
     if (!id) return;
     const fetchMovie = async () => {
@@ -32,6 +33,7 @@ function MovieDetail() {
 
   return (
     <div>
+      <button onClick={() => navigate(-1)}>Go back</button>
       <h1>{movie["#TITLE"]}</h1>
       <p>{movie["#ACTORS"]}</p>
       <img src={movie["#IMG_POSTER"]} alt={movie["#TITLE"]} width={400} />
