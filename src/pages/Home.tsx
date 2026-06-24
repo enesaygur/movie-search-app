c54add60b156a6c2cc1fbff8c8eac8b218eb4215
Enes <enesaygur387@gmail.com>
2026-06-24 13:55:58 +0300

    feat: migrate movie search to TanStack Query

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,30 +2,39 @@ import { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
 import MovieList from "../components/MovieList";
 import { useSearchParams } from "react-router-dom";
-import { useMovieSearch } from "../hooks/useMovieSearch";
-import { useDebounce } from "./../hooks/useDebounce";
+import { useQuery } from "@tanstack/react-query";
+import { searchMovies } from "../services/movieService";
+import { useDebounce } from "../hooks/useDebounce";
 
 function Home() {
   const [searchParams, setSearchParams] = useSearchParams();
   const initialQuery = searchParams.get("q") || "";
   const [query, setQuery] = useState(initialQuery);
   const debounceQuery = useDebounce(query, 500);
-  const { movies, loading, error } = useMovieSearch(debounceQuery);
 
+  const {
+    data: movies,
+    isLoading: loading,
+    error: error,
+  } = useQuery({
+    queryKey: ["movies", debounceQuery],
+    queryFn: () => searchMovies(debounceQuery),
+    enabled: debounceQuery.trim().length > 2,
+  });
   useEffect(() => {
-    if (debounceQuery.trim()) {
-      setSearchParams({ q: debounceQuery });
+    if (query.trim()) {
+      setSearchParams({ q: query });
     } else {
       setSearchParams({});
     }
-  }, [debounceQuery, setSearchParams]);
+  }, [query, setSearchParams]);
 
   return (
     <>
       <h1>Movie Search App</h1>
       <SearchBar value={query} onChange={setQuery} />
       {loading && <p>Loading...</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {error && <p style={{ color: "red" }}>Something went wrong</p>}
       {movies && movies.length > 0 && <MovieList movies={movies} />}
       {movies && movies.length === 0 && <p>No movies found.</p>}
     </>
